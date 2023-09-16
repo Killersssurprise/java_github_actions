eeb225fdebdf8c3fed12e23f9bc45b2615f593f2
Михаил Михайлович <killersssurprise@gmail.com>
2023-09-16 12:10:52 +0300

    Created HelloWorldApp and changed test.yml config

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,6 +1,7 @@
 import org.example.calc.Calculator;
 import org.example.calc.MyCustomDivByZero;
-import org.junit.Assert;
+//import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -11,22 +12,26 @@ public class CalculatorTest {
 
     @Test
     void sumTest(){
-        Assert.assertEquals(Calculator.sum(3,2), 5);
+        Assertions.assertEquals(Calculator.sum(3,2), 5);
+//        Assert.assertEquals(Calculator.sum(3,2), 5);
     }
 
     @Test
     void sumTestFloat1(){
-        Assert.assertEquals(5.4f,Calculator.sum(3.2f,2.2f), 0.00000000001f);
+        Assertions.assertEquals(5.4f,Calculator.sum(3.2f,2.2f), 0.00000000001f);
+//        Assert.assertEquals(5.4f,Calculator.sum(3.2f,2.2f), 0.00000000001f);
     }
 
     @Test
     void divTest() throws MyCustomDivByZero {
-        Assert.assertEquals(Calculator.div(8,2), 4);
+        Assertions.assertEquals(Calculator.div(8,2), 4);
+//        Assert.assertEquals(Calculator.div(8,2), 4);
     }
 
     @Test
     void divTest3() throws MyCustomDivByZero {
-        Assert.assertEquals(Calculator.div(16,2), 8);
+        Assertions.assertEquals(Calculator.div(16,2), 8);
+//        Assert.assertEquals(Calculator.div(16,2), 8);
     }
 
     @Test
@@ -39,12 +44,13 @@ public class CalculatorTest {
 
     @Test
     void multTest(){
-        Assert.assertEquals(Calculator.multiplication(8,2), 16);
+
+        Assertions.assertEquals(Calculator.multiplication(8,2), 16);
     }
 
     @Test
     void subTest(){
-        Assert.assertEquals(Calculator.substraction(8,2), 6);
+        Assertions.assertEquals(Calculator.substraction(8,2), 6);
     }
 
 }
--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,5 +1,5 @@
 import org.example.calc.Calculator;
-import org.junit.Assert;
+//import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
